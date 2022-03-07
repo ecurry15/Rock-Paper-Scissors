f50e3d4a3a8b4f82380fc17839ada9ebabaa2669
Edward <Ecampbell3015@gmail.com>
2022-03-07 13:53:28 -0800

    Compare playerSelction with computerSelction and return a message
    A player will type either rock, paper, or scissors and the function will compare the player's input to the computer's random input and return whether the player won, lost, or tied.

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,34 @@
 let computerPlay = randomValue => {
 let randomPosition = Math.floor((Math.random() * randomValue.length));
 return randomValue[randomPosition];
+};
+
+function computerVsPlayer(playerSelection, computerSelection) {
+  //ROCK Section -----
+if (/rock/gi.test(playerSelection) && computerSelection === "Paper") {
+  return "You Lose. Paper beats Rock"
+} else if (/rock/gi.test(playerSelection) && computerSelection === "Rock") {
+  return "Tie. Try again"
+} else if (/rock/gi.test(playerSelection) && computerSelection === "Scissors") {
+  return "You Win! Rock beats Scissors"
+  //PAPER SECTION -----
+} else if (/paper/gi.test(playerSelection) && computerSelection === "Scissors") {
+  return "You Lose. Scissors beats Paper"
+} else if (/paper/gi.test(playerSelection) && computerSelection === "Paper") {
+  return "Tie. Try again"
+} else if (/paper/gi.test(playerSelection) && computerSelection === "Rock") {
+  return "You Win! Paper beats Rock"
+  //SCISSORS SECTION -----
+} else if (/scissors/gi.test(playerSelection) && computerSelection === "Rock") {
+  return "You Lose. Rock beats Scissors"
+} else if (/scissors/gi.test(playerSelection) && computerSelection === "Scissors") {
+  return "Tie. Try again"
+} else if (/scissors/gi.test(playerSelection) && computerSelection === "Paper") {
+  return "You Win! Scissors beats Paper"
 }
-console.log(computerPlay(["Rock", "Paper", "Scissors"]));
+};
+
+
+let playerSelection = "Scissors";
+let computerSelection = computerPlay(["Rock", "Paper", "Scissors"]);
+console.log(computerVsPlayer(playerSelection, computerSelection));
