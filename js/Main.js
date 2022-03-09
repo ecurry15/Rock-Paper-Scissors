54a85e9db36d7b14c0022473f5a4a97798e87090
Edward <Ecampbell3015@gmail.com>
2022-03-08 18:53:17 -0800

    Track the player's score for 5 games and return whether the player won theset or not
    Code is missing a function to log the results of each round to the console

--- js/Main.js
+++ js/Main.js
@@ -28,7 +28,29 @@ if (/rock/gi.test(playerSelection) && computerSelection === "Paper") {
 }
 };
 
-
-let playerSelection = "Scissors";
+function game() {
+let playerScore = [];
+let computerScore = [];
+let ties = [];
+  let tally = [];
+  for (let i = 0; i < 5; i++) {
+let playerSelection = window.prompt("Type Rock, Paper, or Scissors");
 let computerSelection = computerPlay(["Rock", "Paper", "Scissors"]);
-console.log(computerVsPlayer(playerSelection, computerSelection));
+if (/win/gi.test(computerVsPlayer(playerSelection, computerSelection))) {
+playerScore.push(1);
+} else if (/lose/gi.test(computerVsPlayer(playerSelection, computerSelection))) {
+  computerScore.push(1);
+} else if (/tie/gi.test(computerVsPlayer(playerSelection, computerSelection))) {
+  ties.push(1);
+}
+}
+if (playerScore.length > computerScore.length) {
+  return "Congrats! You won the game";
+} else if (computerScore.length > playerScore.length) {
+  return "Bummer, you lost this game";
+} else if (playerScore.length === computerScore.length) {
+return "It was a tie game, let's play again!"
+}
+}
+console.log(game())
+
